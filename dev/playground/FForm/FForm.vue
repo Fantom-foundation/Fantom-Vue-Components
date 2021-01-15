577980b0b92518edee1e1384f1327d204fe25738
Pavel Spacil <pavel.spacil@gmail.com>
2021-01-15 13:53:47 +0100

    chore(f-form): create playground

---
+++ dev/playground/FForm/FForm.vue
@@ -0,0 +1,137 @@
+<template>
+    <section>
+        <h1 class="h2">Form</h1>
+
+        <f-form center-form @submit="onFormSubmit" class="grid">
+            <fieldset class="col-8">
+                <legend>Test form 2</legend>
+                <div class="fform_fieldset_body">
+                    <div class="fform_row fl">
+                        <f-input label="Input 1" v-model="values.input1" validate-on-input :validator="inputValidator">
+                            <template #bottom="sProps">
+                                <f-message v-show="sProps.showErrorMessage" type="error" role="alert">
+                                    Fill input 1
+                                </f-message>
+                            </template>
+                        </f-input>
+                        <f-input label="Input 2" v-model="values.input2" validate-on-input :validator="inputValidator">
+                            <template #bottom="sProps">
+                                <f-message v-show="sProps.showErrorMessage" type="error" role="alert">
+                                    Fill input 2
+                                </f-message>
+                            </template>
+                        </f-input>
+                    </div>
+                    <div class="fform_row">
+                        <f-option type="checkbox" label="Checkbox 1" v-model="values.checkbox1" />
+                        <f-option type="checkbox" label="Checkbox 2" v-model="values.checkbox2" />
+                    </div>
+                    <div class="fform_row">
+                        <f-option type="checkbox" label="Checkbox multi 1" v-model="values.checkboxMulti" value="10" />
+                        <f-option type="checkbox" label="Checkbox multi 2" v-model="values.checkboxMulti" value="20" />
+                        <f-option type="checkbox" label="Checkbox multi 3" v-model="values.checkboxMulti" value="30" />
+                    </div>
+                    <div class="fform_row tea-center">
+                        <f-option type="radio" checked v-model="values.radio1" label="yes" name="rad1" value="10" />
+                        <f-option type="radio" v-model="values.radio1" label="no" name="rad1" value="20" />
+                        <f-option type="radio" v-model="values.radio1" label="ok" name="rad1" value="30" />
+                    </div>
+                    <div class="fform_row">
+                        <f-dropdown-listbox v-model="values.dropdown1" :data="data.dropdown" />
+                    </div>
+                    <div class="fform_row">
+                        <f-listbox v-model="values.listbox1" :data="data.listbox" value="300" />
+                    </div>
+                    <div class="fform_row">
+                        <f-select v-model="values.select1" label="Default" :data="data.select" />
+                    </div>
+                </div>
+            </fieldset>
+            <div>
+                <f-button type="submit" label="Submit" />
+            </div>
+        </f-form>
+    </section>
+</template>
+
+<script>
+import FForm from '@/components/FForm/FForm.vue';
+import FInput from '@/components/FInput/FInput.vue';
+import FOption from '@/components/FOption/FOption.vue';
+import FSelect from '@/components/FSelect/FSelect.vue';
+import FButton from '@/components/FButton/FButton.vue';
+import FDropdownListbox from '@/components/FDropdownListbox/FDropdownListbox.vue';
+import FListbox from '@/components/FListbox/FListbox.vue';
+import FMessage from '@/components/FMessage/FMessage.vue';
+
+export default {
+    name: 'FFormPlayground',
+
+    components: { FMessage, FListbox, FDropdownListbox, FButton, FSelect, FOption, FInput, FForm },
+
+    data() {
+        return {
+            data: {
+                select: [
+                    { label: 'Option 1', value: 1 },
+                    { label: 'Option 2', value: 2 },
+                    { label: 'Option 3', value: 3 },
+                ],
+                dropdown: [
+                    { label: 'item 1', value: '10' },
+                    { label: 'item 2', id: 'myid', value: '20' },
+                    { label: 'item 3 Lorem ipsum', value: '30' },
+                    { label: 'item 4', value: '40' },
+                    { label: 'item 5', value: '50' },
+                    { label: 'item 6', value: '60' },
+                ],
+                listbox: [
+                    { label: 'item 100', value: '100' },
+                    { label: 'item 200', id: 'myid', value: '200' },
+                    { label: 'item 300 Lorem ipsum', value: '300' },
+                ],
+            },
+            values: {
+                input1: 'value',
+                input2: '',
+                checkbox1: false,
+                checkbox2: true,
+                checkboxMulti: [],
+                radio1: '10',
+                dropdown1: '',
+                listbox1: '300',
+                select1: '',
+            },
+        };
+    },
+
+    methods: {
+        inputValidator(_value) {
+            return !!_value.trim();
+        },
+
+        updateData() {
+            const { values } = this;
+
+            values.checkbox1 = true;
+            values.checkboxMulti = ['10', '20'];
+            values.dropdown1 = '40';
+            values.listbox1 = '200';
+        },
+
+        onFormSubmit() {
+            console.log('onFormSubmit2', this.dt);
+            this.updateData();
+        },
+
+        onFormChange(_data) {
+            console.log('onFormChange', _data);
+            console.log(_data.eTarget.name);
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+@import 'fform';
+</style>
--- dev/serve.vue
+++ dev/serve.vue
@@ -1,39 +1,46 @@
 <template>
-    <div id="app">
-        <h1>test</h1>
+    <main id="app" class="container">
+        <h1>Playground</h1>
 
-        <f-tabs aria-label="Default tabs">
-            <template #tab1-title><b>Bold</b></template>
-            <template #tab2-title><i>Italic</i></template>
-            <template #tab3-title><span>&#9742; With icon</span></template>
+        <section hidden>
+            <h1 class="h2">Tabs</h1>
+            <f-tabs aria-label="Default tabs">
+                <template #tab1-title><b>Bold</b></template>
+                <template #tab2-title><i>Italic</i></template>
+                <template #tab3-title><span>&#9742; With icon</span></template>
 
-            <f-tab title-slot="tab1-title">
-                Tab 1 content
-            </f-tab>
-            <f-tab title-slot="tab2-title">
-                Tab 2 content
-            </f-tab>
-            <f-tab title-slot="tab3-title">
-                Tab 3 content
-            </f-tab>
-            <f-tab title="Tab 4">
-                Tab 4 content
-            </f-tab>
-        </f-tabs>
-    </div>
+                <f-tab title-slot="tab1-title">
+                    Tab 1 content
+                </f-tab>
+                <f-tab title-slot="tab2-title">
+                    Tab 2 content
+                </f-tab>
+                <f-tab title-slot="tab3-title">
+                    Tab 3 content
+                </f-tab>
+                <f-tab title="Tab 4">
+                    Tab 4 content
+                </f-tab>
+            </f-tabs>
+        </section>
+
+        <f-form-playground />
+    </main>
 </template>
 
 <script>
 import Vue from 'vue';
 import FTabs from '@/components/FTabs/FTabs.vue';
 import FTab from '@/components/FTabs/FTab.vue';
+import FFormPlayground from './playground/FForm/FForm.vue';
 
 // import components from '../src/entry.js';
 // components.install(Vue);
 
 export default Vue.extend({
     name: 'ServeDev',
-    components: { FTab, FTabs },
+
+    components: { FFormPlayground, FTab, FTabs },
 });
 </script>
 
