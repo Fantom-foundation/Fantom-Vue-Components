90ee804f66791aab66fa6be51bbef023cad1531e
Pavel Spacil <pavel.spacil@gmail.com>
2021-01-24 07:00:47 +0100

    feat(form-input): create skeleton of form-input mixin

---
+++ src/mixins/form-input.js
@@ -0,0 +1,20 @@
+import { getUniqueId } from '../utils/index.js';
+
+/**
+ * Common props and methods for inputs with validation
+ */
+export const formInputMixin = {
+    props: {
+        /** Id of element that represents label for the component */
+        labeledBy: {
+            type: String,
+            default: '',
+        },
+    },
+
+    data() {
+        return {
+            labeledById: this.labeledBy || getUniqueId(),
+        };
+    },
+};
