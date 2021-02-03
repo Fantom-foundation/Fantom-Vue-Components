d815b44645406ef8a018bcce997cd1f5811ab435
Pavel Spacil <pavel.spacil@gmail.com>
2021-02-03 09:30:55 +0100

    feat(f-svg-icon): create component

---
+++ src/components/FSvgIcon/FSvgIcon.stories.js
@@ -0,0 +1,69 @@
+import { withA11y } from '@storybook/addon-a11y';
+
+import FSvgIcon from './FSvgIcon.vue';
+import IconFantom from '../icons/IconFantom.vue';
+import IconTimes from '../icons/IconTimes.vue';
+import IconPresent from '../icons/IconPresent.vue';
+
+export default {
+    title: 'FSvgIcon',
+    component: FSvgIcon,
+    decorators: [withA11y],
+};
+
+export const Default = () => ({
+    components: { FSvgIcon, IconTimes, IconPresent, IconFantom },
+    template: `
+        <div>
+            <f-svg-icon><icon-fantom /></f-svg-icon>
+            <f-svg-icon><icon-times /></f-svg-icon>
+            <f-svg-icon><icon-present /></f-svg-icon>
+        </div>
+    `,
+});
+
+export const Size = () => ({
+    components: { FSvgIcon, IconTimes, IconPresent, IconFantom },
+    template: `
+        <div>
+            <f-svg-icon size="16px"><icon-fantom /></f-svg-icon>
+            <f-svg-icon><icon-fantom /></f-svg-icon>
+            <f-svg-icon style="--fsvgicon-width: 48px;--fsvgicon-height: 48px;"><icon-fantom /></f-svg-icon>
+            <f-svg-icon width="64px" height="64px"><icon-fantom /></f-svg-icon>
+        </div>
+    `,
+});
+
+export const Color = () => ({
+    components: { FSvgIcon, IconTimes, IconPresent, IconFantom },
+    template: `
+        <div>
+            <f-svg-icon color="#00c"><icon-fantom /></f-svg-icon>
+            <f-svg-icon color="#c00"><icon-times /></f-svg-icon>
+            <f-svg-icon style="--fsvgicon-color: #0c0"><icon-present /></f-svg-icon>
+        </div>
+    `,
+});
+
+export const Original = () => ({
+    components: { FSvgIcon, IconTimes, IconPresent, IconFantom },
+    template: `
+        <div>
+            <f-svg-icon original><icon-fantom /></f-svg-icon>
+            <f-svg-icon original><icon-times /></f-svg-icon>
+            <f-svg-icon original><icon-present /></f-svg-icon>
+        </div>
+    `,
+});
+
+export const Rotate = () => ({
+    components: { FSvgIcon, IconTimes, IconPresent, IconFantom },
+    template: `
+        <div>
+            <f-svg-icon rotate="0deg" size="32px"><icon-fantom /></f-svg-icon>
+            <f-svg-icon rotate="90deg" size="32px"><icon-fantom /></f-svg-icon>
+            <f-svg-icon rotate="180deg" size="32px"><icon-fantom /></f-svg-icon>
+            <f-svg-icon rotate="270deg" size="32px"><icon-fantom /></f-svg-icon>
+        </div>
+    `,
+});
---
+++ src/components/FSvgIcon/FSvgIcon.vue
@@ -0,0 +1,79 @@
+<template>
+    <span class="fsvgicon" :class="classes" :style="styles">
+        <span class="fsvgicon-icon" :style="iconStyles"><slot /></span>
+    </span>
+</template>
+
+<script>
+/**
+ * Simple SVG wrapper
+ */
+export default {
+    name: 'FSvgIcon',
+
+    props: {
+        /** Color of svg icon. By default color is inherited */
+        color: {
+            type: String,
+            default: '',
+        },
+        /** Preserve original colors of svg */
+        original: {
+            type: Boolean,
+            default: false,
+        },
+        /** Size of icon. Sets width and height by the same value */
+        size: {
+            type: [String, Number],
+            default: '',
+        },
+        /** Width of icon */
+        width: {
+            type: [String, Number],
+            default: '',
+        },
+        /** Height of icon */
+        height: {
+            type: [String, Number],
+            default: '',
+        },
+        /** Rotate icon by `rotate` degrees. For example `'90deg'` */
+        rotate: {
+            type: String,
+            default: '',
+        },
+    },
+
+    data() {
+        return {};
+    },
+
+    computed: {
+        classes() {
+            return {
+                'fsvgicon-original': this.original,
+            };
+        },
+
+        styles() {
+            const { size } = this;
+
+            return {
+                color: this.color,
+                width: size || this.width,
+                height: size || this.height,
+            };
+        },
+
+        iconStyles() {
+            return {
+                transform: `rotate(${this.rotate})`,
+            };
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+@use "style";
+</style>
