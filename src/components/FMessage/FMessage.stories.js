403053cb231bbaa128bda3473f13a36410c912ac
Pavel Spacil <pavel.spacil@gmail.com>
2021-02-08 15:02:50 +0100

    feat(f-message): add support for hiding and animating message
    create stories

--- src/components/FMessage/FMessage.stories.js
+++ src/components/FMessage/FMessage.stories.js
@@ -27,6 +27,45 @@ export const Default = () => ({
     methods: { validator },
 });
 
+export const Animate = () => ({
+    components: { FMessage },
+    template: `
+        <div>
+            <f-message animate hide-on-click type="success"><b>Success</b> message</f-message>
+            <f-message animate hide-on-click animation-in="slide-right-enter-active" animation-out="slide-left-leave-active" type="error"><b>Error</b> message</f-message>
+            <f-message animate hide-on-click animation-in="scale-center-enter-active" animation-out="scale-center-leave-active" type="info"><b>Info</b> message</f-message>
+            <f-message animate hide-on-click animation-in="slide-left-enter-active" animation-out="slide-right-leave-active" type="warning"><b>Warning</b> message</f-message>
+        </div>
+    `,
+    methods: { validator },
+});
+
+export const Hide = () => ({
+    components: { FMessage },
+    template: `
+        <div>
+            <h3>Hide on message click</h3>
+            <f-message hide-on-click animate type="success"><b>Success</b> message</f-message>
+            <f-message hide-on-click animate animation-in="slide-right-enter-active" animation-out="slide-left-leave-active" type="error"><b>Error</b> message</f-message>
+            <f-message hide-on-click animate animation-in="scale-center-enter-active" animation-out="scale-center-leave-active" type="info"><b>Info</b> message</f-message>
+            <f-message hide-on-click animate animation-in="slide-left-enter-active" animation-out="slide-right-leave-active" type="warning"><b>Warning</b> message</f-message>
+
+            <h3>Hide on 'close' button click</h3>
+            <f-message hide-on-close-button animate type="success"><b>Success</b> message</f-message>
+            <f-message hide-on-close-button animate animation-in="slide-right-enter-active" animation-out="slide-left-leave-active" type="error"><b>Error</b> message</f-message>
+            <f-message hide-on-close-button animate animation-in="scale-center-enter-active" animation-out="scale-center-leave-active" type="info"><b>Info</b> message</f-message>
+            <f-message hide-on-close-button animate animation-in="slide-left-enter-active" animation-out="slide-right-leave-active" type="warning"><b>Warning</b> message</f-message>
+
+            <h3>Hide after 3 seconds</h3>
+            <f-message :hide-after="3000" animate type="success"><b>Success</b> message</f-message>
+            <f-message :hide-after="3000" animate animation-in="slide-right-enter-active" animation-out="slide-left-leave-active" type="error"><b>Error</b> message</f-message>
+            <f-message :hide-after="3000" animate animation-in="scale-center-enter-active" animation-out="scale-center-leave-active" type="info"><b>Info</b> message</f-message>
+            <f-message :hide-after="3000" animate animation-in="slide-left-enter-active" animation-out="slide-right-leave-active" type="warning"><b>Warning</b> message</f-message>
+        </div>
+    `,
+    methods: { validator },
+});
+
 export const Slots = () => ({
     components: { FMessage },
     template: `
--- src/components/FMessage/FMessage.vue
+++ src/components/FMessage/FMessage.vue
@@ -1,39 +1,67 @@
 <template>
-    <div class="fmessage" :class="[classes, typeClass]">
-        <!-- @slot Default to `type` prop -->
-        <slot name="prefix">
-            <template v-if="withIcon">
-                <f-svg-icon v-if="type === 'success'" size="16px" aria-hidden="true">
-                    <icon-check-circle />
-                </f-svg-icon>
-                <f-svg-icon v-else-if="type === 'error' || type === 'warning'" size="16px" aria-hidden="true">
-                    <icon-exclamation-circle />
-                </f-svg-icon>
-                <f-svg-icon v-else-if="type === 'info'" size="16px" aria-hidden="true">
-                    <icon-info-circle />
-                </f-svg-icon>
+    <transition :enter-active-class="dAnimationIn" :leave-active-class="dAnimationOut" @after-leave="onAfterLeave">
+        <div
+            v-if="isVisible"
+            :role="alert ? 'alert' : null"
+            class="fmessage"
+            :class="[classes, typeClass]"
+            @click="onClick"
+        >
+            <!-- @slot Default to `type` prop -->
+            <slot name="prefix">
+                <template v-if="withIcon">
+                    <f-svg-icon v-if="type === 'success'" aria-hidden="true" class="fmessage_icon">
+                        <icon-check-circle />
+                    </f-svg-icon>
+                    <f-svg-icon
+                        v-else-if="type === 'error' || type === 'warning'"
+                        aria-hidden="true"
+                        class="fmessage_icon"
+                    >
+                        <icon-exclamation-circle />
+                    </f-svg-icon>
+                    <f-svg-icon v-else-if="type === 'info'" aria-hidden="true" class="fmessage_icon">
+                        <icon-info-circle />
+                    </f-svg-icon>
+                </template>
+            </slot>
+            <span class="fmessage_body"><slot></slot></span>
+            <slot name="suffix"></slot>
+            <template v-if="hideOnCloseButton">
+                <slot name="close_button">
+                    <f-button @click.native="hide()" size="small" round class="fmessage_closebtn">
+                        <f-svg-icon size="12px"><icon-times /></f-svg-icon>
+                    </f-button>
+                </slot>
             </template>
-        </slot>
-        <span class="fmessage_body"><slot></slot></span>
-        <slot name="suffix"></slot>
-    </div>
+        </div>
+    </transition>
 </template>
 
 <script>
 import { helpersMixin } from '../../mixins/helpers.js';
 import FSvgIcon from '../FSvgIcon/FSvgIcon.vue';
-import IconCheckCircle from '@/components/icons/IconCheckCircle.vue';
-import IconExclamationCircle from '@/components/icons/IconExclamationCircle.vue';
+import IconCheckCircle from '../icons/IconCheckCircle.vue';
+import IconExclamationCircle from '../icons/IconExclamationCircle.vue';
 import IconInfoCircle from '../icons/IconInfoCircle.vue';
+import { getUniqueId } from '../../utils/index.js';
+import IconTimes from '../icons/IconTimes.vue';
+import FButton from '@/components/FButton/FButton.vue';
+// import { returnFocus, setReceiveFocusFromAttr } from '@/utils/aria.js';
+// import { getComputedStyle } from '@/utils/css.js';
 
 /**
  * Component for displaying simple messages.
  */
 export default {
-    components: { IconExclamationCircle, IconCheckCircle, FSvgIcon, IconInfoCircle },
+    components: { FButton, IconTimes, IconExclamationCircle, IconCheckCircle, FSvgIcon, IconInfoCircle },
     mixins: [helpersMixin],
 
     props: {
+        id: {
+            type: String,
+            default: getUniqueId(),
+        },
         /**
          * Type of message.
          *
@@ -47,11 +75,54 @@ export default {
                 return ['success', 'info', 'warning', 'error'].indexOf(_value) !== -1;
             },
         },
+        /** Signals that message is an alert message (has role="alert" attribute) */
+        alert: {
+            type: Boolean,
+            default: false,
+        },
         /** Use icon with message. */
         withIcon: {
             type: Boolean,
             default: false,
         },
+        /** Message will be hidden by clicking on it */
+        hideOnClick: {
+            type: Boolean,
+            default: false,
+        },
+        /** Message will be hidden by clicking on 'close' button */
+        hideOnCloseButton: {
+            type: Boolean,
+            default: false,
+        },
+        /** Signals using of animations on show/hide */
+        animate: {
+            type: Boolean,
+            default: false,
+        },
+        /** Animation that starts when the message is just about to show */
+        animationIn: {
+            type: String,
+            default: 'fade-enter-active',
+        },
+        /** Animation that starts when the message is just about to hide */
+        animationOut: {
+            type: String,
+            default: 'fade-leave-active',
+        },
+        /** Hide message after this amout of milliseconds. 0 means no auto hiding */
+        hideAfter: {
+            type: Number,
+            default: 0,
+        },
+    },
+
+    data() {
+        return {
+            isVisible: !this.animate,
+            dAnimationIn: this.animationIn,
+            dAnimationOut: this.animationOut,
+        };
     },
 
     computed: {
@@ -68,9 +139,96 @@ export default {
         },
     },
 
+    created() {
+        /** `hideAfter` timeout id. */
+        this._hideAfterId = -1;
+
+        if (this.animate) {
+            this.show();
+        }
+    },
+
+    beforeDestroy() {
+        if (this.isVisible) {
+            this.hide();
+        }
+    },
+
     methods: {
+        show(_animationIn) {
+            if (!this.isVisible) {
+                if (this.animate) {
+                    if (_animationIn) {
+                        this.dAnimationIn = _animationIn;
+                    } else {
+                        this.dAnimationIn = this.animationIn;
+                    }
+
+                    this.$nextTick(() => {
+                        this.isVisible = true;
+                    });
+                } else {
+                    this.isVisible = true;
+                }
+
+                this.startHideAfterTimeout();
+            }
+        },
+
+        hide(_animationOut) {
+            if (this.isVisible) {
+                if (this.animate) {
+                    if (_animationOut) {
+                        this.dAnimationOut = _animationOut;
+                    } else {
+                        this.dAnimationOut = this.animationOut;
+                    }
+
+                    this.$nextTick(() => {
+                        this.isVisible = false;
+                    });
+                } else {
+                    this.isVisible = false;
+                }
+            }
+        },
+
+        toggle() {
+            if (!this.isVisible) {
+                this.show();
+            } else {
+                this.hide();
+            }
+        },
+
+        startHideAfterTimeout() {
+            if (this.hideAfter > 0) {
+                this.clearHideAfterTimeout();
+
+                this._hideAfterId = setTimeout(() => {
+                    this.hide();
+                }, this.hideAfter);
+            }
+        },
+
+        clearHideAfterTimeout() {
+            if (this._hideAfterId > -1) {
+                clearTimeout(this._hideAfterId);
+            }
+        },
+
         getMessage() {
             return this.$slots.default ? this.$slots.default[0].text : '';
+        },
+
+        onClick() {
+            if (this.hideOnClick && !this.hideOnCloseButton) {
+                this.hide();
+            }
+        },
+
+        onAfterLeave() {
+            this.$emit('message-hidden', this.id);
         },
     },
 };
