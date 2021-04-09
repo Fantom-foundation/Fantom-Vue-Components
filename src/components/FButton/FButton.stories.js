537a4f6cc21d73684983eecf4755e8c0e09edbdd
Pavel Spacil <pavel.spacil@gmail.com>
2021-04-09 13:51:14 +0200

    feat(f-button, scss): add color variants

--- src/components/FButton/FButton.stories.js
+++ src/components/FButton/FButton.stories.js
@@ -47,7 +47,7 @@ export const Disabled = () => ({
             <f-button disabled size="large" label="Large" />
             <f-button disabled label="Default" />
             <f-button disabled size="small" label="Small" />
-            <f-button disabled size="mini" label="Mini" />
+            <f-button disabled size="mini" label="Mini" /> class="fbutton-light"
             <h3>Secondary</h3>
             <f-button disabled secondary size="large" label="Large" />
             <f-button disabled secondary label="Default" />
@@ -62,6 +62,56 @@ export const Disabled = () => ({
             <f-button disabled round><f-svg-icon><icon-times /></f-svg-icon></f-button>
             <f-button disabled round secondary><f-svg-icon><icon-times /></f-svg-icon></f-button>
             <f-button disabled round tertiary><f-svg-icon><icon-times /></f-svg-icon></f-button>
+        </div>
+    `,
+});
+
+export const Variants = () => ({
+    components: { FButton, FSvgIcon, IconTimes },
+    template: `
+        <div>
+            <h3><code>.fbutton-red</code></h3>
+            <f-button label="Default" class="btn-red" />
+            <f-button secondary label="Default" class="btn-red" />
+            <f-button tertiary label="Default" class="btn-red" />
+            <br />
+            <f-button disabled label="Default" class="btn-red" />
+            <f-button disabled secondary label="Default" class="btn-red" />
+            <f-button disabled tertiary label="Default" class="btn-red" />
+            <h3><code>.btn-green</code></h3>
+            <f-button label="Default" class="btn-green" />
+            <f-button secondary label="Default" class="btn-green" />
+            <f-button tertiary label="Default" class="btn-green" />
+            <br />
+            <f-button disabled label="Default" class="btn-green" />
+            <f-button disabled secondary label="Default" class="btn-green" />
+            <f-button disabled tertiary label="Default" class="btn-green" />
+            <h3><code>.btn-orange</code></h3>
+            <f-button label="Default" class="btn-orange" />
+            <f-button secondary label="Default" class="btn-orange" />
+            <f-button tertiary label="Default" class="btn-orange" />
+            <br />
+            <f-button disabled label="Default" class="btn-orange" />
+            <f-button disabled secondary label="Default" class="btn-orange" />
+            <f-button disabled tertiary label="Default" class="btn-orange" />
+            <h3><code>.btn-grey</code></h3>
+            <f-button label="Default" class="btn-grey" />
+            <f-button secondary label="Default" class="btn-grey" />
+            <f-button tertiary label="Default" class="btn-grey" />
+            <br />
+            <f-button disabled label="Default" class="btn-grey" />
+            <f-button disabled secondary label="Default" class="btn-grey" />
+            <f-button disabled tertiary label="Default" class="btn-grey" />
+            <h3><code>.btn-light, .darktheme -> fbutton</code></h3>
+            <div class="bac-grey-8 pa-5 darktheme">
+                <f-button label="Default" />
+                <f-button secondary label="Default" class="btn-light" />
+                <f-button tertiary label="Default" class="btn-light" />
+                <br />
+                <f-button disabled label="Default" />
+                <f-button disabled secondary label="Default" class="btn-light" />
+                <f-button disabled tertiary label="Default" class="btn-light" />
+            </div>
         </div>
     `,
 });
