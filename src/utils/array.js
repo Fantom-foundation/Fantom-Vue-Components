19d4d587370cf74c2a6804d7710874136c543bff
Pavel Spacil <pavel.spacil@gmail.com>
2021-04-09 14:07:47 +0200

    feat(utils): create array utils

---
+++ src/utils/array.js
@@ -0,0 +1,28 @@
+/**
+ * Shuffles an array.
+ *
+ * @param {Array} _array
+ */
+import { isArray } from './index.js';
+
+export function shuffle(_array) {
+    for (let i = _array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+        [_array[i], _array[j]] = [_array[j], _array[i]];
+    }
+}
+
+/**
+ * @param {array} _array1
+ * @param {array} _array2
+ * @return {boolean}
+ */
+export function arrayEquals(_array1, _array2) {
+    return (
+        isArray(_array1) &&
+        isArray(_array2) &&
+        _array1.length === _array2.length &&
+        _array1.every((_val, _index) => _val === _array2[_index])
+    );
+}
