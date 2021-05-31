c590c4c98d6c9012841ed6fa335edd59141c84d9
Pavel Spacil <pavel.spacil@gmail.com>
2021-05-31 07:57:55 +0200

    feat(f-combo-box): create component

---
+++ src/components/FComboBox/FComboBox.stories.js
@@ -0,0 +1,384 @@
+import FComboBox from '@/components/FComboBox/FComboBox.vue';
+import FButton from '../FButton/FButton.vue';
+import { clone } from '@/utils';
+import FListbox from '@/components/FListbox/FListbox.vue';
+
+const comboboxData = [
+    {
+        value: 2,
+        label: 'Tani',
+    },
+    {
+        value: 11,
+        label: 'Fonsie',
+    },
+    {
+        value: 5,
+        label: 'Slade',
+    },
+    {
+        value: 19,
+        label: 'Elladine',
+    },
+    {
+        value: 13,
+        label: 'Shandie',
+        // label: 'Shandie looooooooooooooooooooooooooooooooooooooooooooooooooong',
+    },
+    {
+        value: 14,
+        label: 'Noach',
+    },
+    {
+        value: 9,
+        label: 'Shela',
+    },
+    {
+        value: 6,
+        label: 'Lura',
+    },
+    {
+        value: 12,
+        label: 'Lia',
+    },
+    {
+        value: 15,
+        label: 'Frayda',
+    },
+];
+
+const comboboxData2 = [
+    { value: 1, label: 'Armstrong' },
+    { value: 2, label: 'Laurene' },
+    { value: 3, label: 'Shelley' },
+    { value: 4, label: 'Sukey' },
+    { value: 5, label: 'Scarlett' },
+    { value: 6, label: 'Phyllida' },
+    { value: 7, label: 'Adara' },
+    { value: 8, label: 'Brandon' },
+    { value: 9, label: 'Hedwiga' },
+    { value: 10, label: 'Misha' },
+    { value: 11, label: 'Maurita' },
+    { value: 12, label: 'Jamill' },
+    { value: 13, label: 'Humfrid' },
+    { value: 14, label: 'Cleo' },
+    { value: 15, label: 'Beverly' },
+    { value: 16, label: 'Concordia' },
+    { value: 17, label: 'Brandi' },
+    { value: 18, label: 'Sebastien' },
+    { value: 19, label: 'Rosabelle' },
+    { value: 20, label: 'Rycca' },
+    { value: 21, label: 'Bevvy' },
+    { value: 22, label: 'Dorena' },
+    { value: 23, label: 'Goran' },
+    { value: 24, label: 'Fiann' },
+    { value: 25, label: 'Eva' },
+    { value: 26, label: 'Dionis' },
+    { value: 27, label: 'Terrence' },
+    { value: 28, label: 'Amalia' },
+    { value: 29, label: 'Ford' },
+    { value: 30, label: 'Tommy' },
+];
+
+function fetchPagedComboBoxData(_timeout = 1000, _pagination) {
+    return new Promise(_resolve =>
+        setTimeout(() => {
+            let ldata = clone(comboboxData2);
+            const filterText = _pagination.filterText.trim().toLowerCase();
+
+            ldata = ldata.filter(_item => _item.label.toLowerCase().indexOf(filterText) > -1);
+
+            _resolve({
+                totalItems: ldata.length,
+                items: ldata.slice(
+                    (_pagination.currPage - 1) * _pagination.perPage,
+                    _pagination.currPage * _pagination.perPage
+                ),
+            });
+        }, _timeout)
+    );
+}
+
+/**
+ * Example of FComboBox wrapper.
+ */
+const CustomComboBox = {
+    components: { FComboBox },
+
+    template: `
+        <f-combo-box
+            v-bind="{ ...$attrs, ...$props }"
+            v-on="$listeners"
+            select-mode
+            strategy="remote"
+            focus
+            :data="dData"
+            :transform-data-func="onTransformData"
+            :throttle-input-interval="250"
+            @page-change="onPageChange"
+            @change="$emit('input', $event)"
+        >
+            <!-- copy slots -->
+            <template v-for="(index, name) in $scopedSlots" v-slot:[name]="data">
+                <slot :name="name" v-bind="data"></slot>
+            </template>
+        </f-combo-box>
+    `,
+
+    inheritAttrs: false,
+
+    props: {
+        /**
+         * Listbox's items
+         * @type {FListboxItem[]}
+         */
+        data: { ...FListbox.props.data },
+    },
+
+    data() {
+        return {
+            dData: this.data,
+        };
+    },
+
+    watch: {
+        data(_value) {
+            this.dData = _value;
+        },
+    },
+
+    methods: {
+        onPageChange(_event) {
+            this.dData = fetchPagedComboBoxData(1000, _event);
+        },
+
+        onTransformData(_data) {
+            return {
+                totalItems: parseInt(_data.totalItems),
+                data: _data.items,
+            };
+        },
+    },
+};
+
+export default {
+    title: 'FComboBox',
+    component: FComboBox,
+};
+
+export const Default = () => ({
+    components: { FComboBox },
+    template: `
+        <div>
+            <f-combo-box :data="data" @change="value = $event" />
+            value: {{ value }}
+        </div>
+    `,
+    data() {
+        return {
+            data: clone(comboboxData),
+            value: 0,
+        };
+    },
+});
+
+export const TextIsValue = () => ({
+    components: { FComboBox },
+    template: `
+        <div>
+            <f-combo-box text-is-value :data="data" @change="value = $event" />
+            value: {{ value }}
+        </div>
+    `,
+    data() {
+        return {
+            data: clone(comboboxData),
+            value: 0,
+        };
+    },
+});
+
+export const SelectMode = () => ({
+    components: { FComboBox },
+    template: `
+        <div>
+            <f-combo-box select-mode :data="data" @change="value = $event" />
+            value: {{ value }}
+            <h4>readonly</h4>
+            <f-combo-box select-mode readonly :data="data" @change="value2 = $event" />
+            value: {{ value2 }}
+        </div>
+    `,
+    data() {
+        return {
+            data: clone(comboboxData),
+            value: 0,
+            value2: 0,
+        };
+    },
+});
+
+export const RemoteData = () => ({
+    components: { CustomComboBox },
+    template: `
+        <div>
+            <custom-combo-box @change="value = $event" />
+            value: {{ value }}
+            <p class="mat-3 co-grey-5">See source code for FComboBox stories to see an example of FComboBox wrapper (CustomComboBox).</p>
+        </div>
+    `,
+    data() {
+        return {
+            value: 0,
+        };
+    },
+});
+
+export const NoInlineAutocomplete = () => ({
+    components: { FComboBox, CustomComboBox },
+    template: `
+        <div>
+            <p>Selection is performed only by pressing 'Enter' key</p>
+
+            <f-combo-box :inline-autocomplete="false" select-mode :data="data" @change="value = $event" />
+            value: {{ value }}
+            <h4>readonly</h4>
+            <f-combo-box :inline-autocomplete="false" select-mode readonly :data="data" @change="value2 = $event" />
+            value: {{ value2 }}
+            <h4>remote (CustomComboBox)</h4>
+            <custom-combo-box :inline-autocomplete="false" @change="value3 = $event" />
+            value: {{ value3 }}
+        </div>
+    `,
+    data() {
+        return {
+            data: clone(comboboxData),
+            value: 0,
+            value2: 0,
+            value3: 0,
+        };
+    },
+});
+
+export const Model = () => ({
+    components: { FComboBox, FButton },
+    template: `
+        <div>
+            <f-combo-box v-model="value" select-mode :data="data" />
+            <f-button secondary size="small" @click.native="onButtonClick">Set value to 19</f-button>
+            <br />
+            selected: {{ value }}
+        </div>
+    `,
+    data() {
+        return {
+            data: clone(comboboxData),
+            value: 9,
+        };
+    },
+    methods: {
+        onButtonClick() {
+            this.value = 19;
+        },
+    },
+});
+
+export const FieldSize = () => ({
+    components: { FComboBox },
+    template: `
+        <div>
+            <f-combo-box field-size="large" :data="data" />
+            <f-combo-box :data="data" />
+            <f-combo-box field-size="small" :data="data" />
+        </div>
+    `,
+    data() {
+        return {
+            data: clone(comboboxData),
+        };
+    },
+});
+
+export const Disabled = () => ({
+    components: { FComboBox },
+    template: `
+        <div>
+            <f-combo-box disabled field-size="large" :data="data" />
+            <f-combo-box disabled select-mode :data="data" />
+            <f-combo-box disabled field-size="small" :data="data" />
+        </div>
+    `,
+    data() {
+        return {
+            data: clone(comboboxData),
+        };
+    },
+});
+
+export const Invalid = () => ({
+    components: { FComboBox },
+    template: `
+        <div>
+            <f-combo-box invalid field-size="large" :data="data" />
+            <f-combo-box invalid :data="data" />
+            <f-combo-box invalid field-size="small" :data="data" />
+        </div>
+    `,
+    data() {
+        return {
+            data: clone(comboboxData),
+        };
+    },
+});
+
+export const Validation = () => ({
+    components: { FComboBox, FButton },
+    template: `
+        <div>
+            <form action="" @submit="onSubmit">
+                <f-combo-box ref="combobox" :validator="validator" __validate-on-change select-mode :data="data" />
+                <br /><br />
+                <f-button type="submit" size="small">Submit</f-button>
+            </form>
+        </div>
+    `,
+    data() {
+        return {
+            data: clone(comboboxData),
+        };
+    },
+    methods: {
+        validator(_value) {
+            if (_value !== 11) {
+                return 'Fonsie must be selected';
+            }
+
+            return '';
+        },
+        onSubmit(_event) {
+            this.$refs.combobox.validate();
+
+            _event.preventDefault();
+        },
+    },
+});
+
+export const Slot = () => ({
+    components: { FComboBox },
+    template: `
+        <div>
+            <f-combo-box select-mode :data="data">
+                <template v-slot:item="{ item }">
+                    <div class="flex juc-space-between">
+                        {{ item.label }} <i>{{ item.value }}</i>
+                    </div>
+                </template>
+            </f-combo-box>
+        </div>
+    `,
+    data() {
+        return {
+            data: clone(comboboxData),
+        };
+    },
+});
---
+++ src/components/FComboBox/FComboBox.vue
@@ -0,0 +1,494 @@
+<template>
+    <div
+        class="fcombobox"
+        :class="classes"
+        @[clickEvent]="onClick"
+        role="combobox"
+        aria-haspopup="listbox"
+        :aria-owns="listboxId"
+        :aria-expanded="popoverVisible ? 'true' : 'false'"
+        :aria-invalid="validationState.invalid"
+    >
+        <f-input
+            v-model="iValue"
+            ref="input"
+            v-bind="$props"
+            autocomplete="off"
+            aria-autocomplete="list"
+            :input-cont-id="dInputContId"
+            :controls-id="listboxId"
+            :aria-activedescendant="activeDescendant"
+            :readonly="readonly"
+            :validator="null"
+            :invalid="validationState.invalid"
+            @keydown.native="onInputKeydown"
+            @keyup.native="onInputKeyup"
+            @focus="onInputFocus"
+            @blur="onInputBlur"
+            :aria-invalid="validationState.invalid"
+            class="inp-withbutton"
+        >
+            <template v-for="(index, name) in $scopedSlots" v-slot:[name]="data">
+                <slot :name="name" v-bind="data"></slot>
+            </template>
+
+            <template #suffix="sProps">
+                <span @click="onButtonClick">
+                    <slot name="button" v-bind="{ ...sProps, ...slotProps, ...loading }">
+                        <f-button
+                            :size="buttonSize"
+                            :disabled="disabled"
+                            tertiary
+                            round
+                            :title="_('fsearchfield.deleteInputText')"
+                        >
+                            <slot name="icon" v-bind="{ ...sProps, ...slotProps, ...loading }">
+                                <f-svg-icon v-if="!loading" size="1em">
+                                    <icon-chevron-down />
+                                </f-svg-icon>
+                                <span v-else><f-dots-loader size="var(--f-spacer-1, 3px)"/></span>
+                            </slot>
+                        </f-button>
+                    </slot>
+                </span>
+            </template>
+        </f-input>
+
+        <f-window
+            v-if="popoverVisible"
+            ref="popover"
+            popover
+            correct-pos-on-scroll
+            :with-header="false"
+            :stay-in-viewport="false"
+            :attach-to="`#${dInputContId}`"
+            attach-position="auto-vertical-exact"
+            __attach-position="lt lb"
+            :animation-in="animationIn"
+            :animation-out="animationOut"
+            __hide-on-document-mousedown
+            prevent-focus
+            width-as-attach
+            class="fcombobox_fwindow"
+            @window-hide="onPopoverHide"
+        >
+            <f-listbox
+                ref="listbox"
+                :listbox-id="listboxId"
+                label=""
+                info-text=""
+                no-aria-activedescendant
+                v-bind="$attrs"
+                :strategy="strategy"
+                :value="inputValue"
+                :data="listboxData"
+                :filter-text="listboxFilterText"
+                :transform-data-func="transformDataFunc"
+                :select-immediately="inlineAutocomplete"
+                class="fcombobox_flistbox"
+                @component-change="onListboxItemSelected"
+                @page-change="$emit('page-change', $event)"
+                @loading="onLoading"
+                @item-focus="onListboxItemFocus"
+            >
+                <template v-slot="{ item }">
+                    <slot name="item" :item="item">
+                        {{ item.label }}
+                    </slot>
+                </template>
+            </f-listbox>
+        </f-window>
+
+        <slot name="bottom" v-bind="slotProps">
+            <div v-if="validationState.errors.length > 0" :id="errorMsgId" class="ferrormessages">
+                <div
+                    v-for="(msg, idx) in validationState.errors"
+                    :key="`${errorMsgId}_${idx}_err`"
+                    class="ferrormessages_message"
+                >
+                    {{ msg }}
+                </div>
+            </div>
+            <div v-else-if="infoText" :id="infoTextId" class="finfotext">
+                {{ infoText }}
+            </div>
+        </slot>
+    </div>
+</template>
+
+<script>
+// import FSelect from '../FSelect/FSelect.vue';
+import FListbox from '../FListbox/FListbox.vue';
+import FWindow from '../FWindow/FWindow.vue';
+import { popoverAnimationMixin } from '../../mixins/popover-animation.js';
+import { fieldWithButtonMixin } from '../../mixins/field-with-button.js';
+import { translationsMixin } from '../../mixins/translations.js';
+import { clone, defer, getUniqueId, isPromise } from '../../utils/index.js';
+import FInput from '../FInput/FInput.vue';
+import { isKey } from '../../utils/aria.js';
+import FButton from '../FButton/FButton.vue';
+import FSvgIcon from '../FSvgIcon/FSvgIcon.vue';
+import IconChevronDown from '../icons/IconChevronDown.vue';
+import FDotsLoader from '../FDotsLoader/FDotsLoader.vue';
+import { selectMixin } from '../../mixins/select.js';
+import { formInputMixin } from '../../mixins/form-input.js';
+import { helpersMixin } from '../../mixins/helpers.js';
+
+/**
+ * @param {Object} _data
+ * @return {{totalItems: number, data: array}}
+ */
+function defaultTransformDataFunc(_data) {
+    return {
+        totalItems: parseInt(_data.totalItems),
+        data: _data.data,
+    };
+}
+
+/**
+ * Combobox component following wai-aria practicess
+ */
+export default {
+    name: 'FComboBox',
+
+    inheritAttrs: false,
+
+    components: { FDotsLoader, IconChevronDown, FSvgIcon, FButton, FInput, FListbox, FWindow },
+
+    // mixins: [popoverAnimationMixin, translationsMixin, fieldWithButtonMixin, selectMixin, formInputMixin, helpersMixin],
+    mixins: [popoverAnimationMixin, translationsMixin, selectMixin, formInputMixin, helpersMixin],
+
+    model: {
+        prop: 'value',
+        event: 'change',
+    },
+
+    props: {
+        ...FInput.props,
+        // ...FListbox.props,
+        /**
+         * Listbox's items
+         * @type {FListboxItem[]}
+         */
+        data: { ...FListbox.props.data },
+        /** Used for transforming data from promise. Have to return object `{totalItems: number, data: array}` */
+        transformDataFunc: {
+            type: Function,
+            default: defaultTransformDataFunc,
+        },
+        /**
+         * Strategy for handling filtering
+         * `'local'` - use local filtering
+         * `'remote'` - just fire 'page-change' event with info about pagination
+         *
+         * @type {('local')}
+         */
+        strategy: {
+            type: String,
+            default: 'local',
+            validator: function(_value) {
+                return ['local', 'remote'].indexOf(_value) !== -1;
+            },
+        },
+        readonly: {
+            type: Boolean,
+            default: false,
+        },
+        /** Value is one of the listbox items. Same behaviour as native select element */
+        selectMode: {
+            type: Boolean,
+            default: false,
+        },
+        textIsValue: {
+            type: Boolean,
+            default: false,
+        },
+        /** If `true`, select an item only by pressing the 'Enter' key or by clicking on the item */
+        inlineAutocomplete: {
+            type: Boolean,
+            default: true,
+        },
+    },
+
+    data() {
+        return {
+            listboxData: [],
+            selectedItem: {},
+            dInputContId: getUniqueId(),
+            listboxId: getUniqueId(),
+            iValue: '',
+            listboxFilterText: '',
+            activeDescendant: '',
+            popoverVisible: false,
+            hasFocus: false,
+            loading: false,
+        };
+    },
+
+    computed: {
+        ...fieldWithButtonMixin.computed,
+
+        clickEvent() {
+            return this.selectMode ? 'click' : null;
+        },
+
+        classes() {
+            return {
+                'fcombobox-selectmode': this.selectMode,
+                'fcombobox-disabled': this.disabled,
+            };
+        },
+    },
+
+    watch: {
+        value(_value) {
+            this.inputValue = _value;
+
+            this.validate();
+
+            if (!isPromise(this.data)) {
+                const selectedItem = this.data.find(_item => _item.value === _value);
+
+                if (selectedItem && selectedItem.value !== this.selectedItem.value) {
+                    this.selectedItem = selectedItem;
+                    this.iValue = this.selectedItem.label;
+                }
+            }
+        },
+
+        selectedItem(_value) {
+            this.$nextTick(() => {
+                this.$emit('selected', clone(_value));
+                this.$emit('change', this.textIsValue ? _value.label : clone(_value.value));
+            });
+        },
+
+        data: {
+            handler(_value, _oldValue) {
+                const valueIsPromise = isPromise(_value);
+
+                if (!valueIsPromise && isPromise(_oldValue) && this._firstPromise) {
+                    this._firstPromise = false;
+                    return;
+                }
+
+                this.listboxData = _value;
+
+                if (this.selectMode && !valueIsPromise) {
+                    this.setSelectedItem();
+                }
+            },
+            immediate: true,
+        },
+
+        iValue(_value) {
+            if (this.hasFocus) {
+                if (this.textIsValue) {
+                    this.$emit('change', _value);
+                }
+
+                if (!this._listboxItemSelected) {
+                    this.filterData(_value);
+                } else if (!this.readonly && !this.inlineAutocomplete) {
+                    this.$nextTick(() => {
+                        this.listboxFilterText = _value;
+                    });
+                }
+            }
+        },
+    },
+
+    created() {
+        this._listboxItemSelected = false;
+        this._firstPromise = true;
+
+        /*if (this.selectMode) {
+            this.setSelectedItem();
+        }*/
+    },
+
+    methods: {
+        async setSelectedItem() {
+            const data = isPromise(this.data) ? this.transformDataFunc(await this.data).data : this.data;
+            const { value } = this;
+            let selectedItem = data.find(_item => !!_item.selected);
+
+            if (!selectedItem && value !== '') {
+                selectedItem = data.find(_item => _item.value === value);
+            }
+
+            if (!selectedItem) {
+                selectedItem = clone(data[0]);
+            }
+
+            this.selectedItem = selectedItem || {};
+
+            this.iValue = this.selectedItem.label;
+        },
+
+        filterData(_text) {
+            this.showPopover();
+
+            this.listboxFilterText = _text;
+            // this.listboxData = this.data.filter(_item => _item.label.indexOf(_text) > -1);
+
+            defer(this.focusFirstListboxItem);
+        },
+
+        focusFirstListboxItem() {
+            const { listbox } = this.$refs;
+
+            if (listbox) {
+                listbox.focusFirstItem();
+            }
+        },
+
+        showPopover() {
+            if (this.popoverVisible) {
+                return;
+            }
+
+            this.popoverVisible = true;
+
+            this.$nextTick(() => {
+                const { popover } = this.$refs;
+
+                if (popover) {
+                    popover.show();
+                    // defer(this.focusFirstListboxItem);
+                }
+            });
+        },
+
+        hidePopover() {
+            const { popover } = this.$refs;
+
+            if (popover && this.popoverVisible) {
+                popover.hide();
+            }
+        },
+
+        onPopoverHide(_event) {
+            this.$emit('window-hide', _event);
+
+            this.$nextTick(() => {
+                this.popoverVisible = false;
+            });
+        },
+
+        /**
+         * @param {FListboxItem} _item
+         * @param {string} _selectionAction
+         */
+        onListboxItemSelected(_item, _selectionAction) {
+            this._listboxItemSelected = true;
+            this.iValue = _item.label;
+            this.selectedItem = _item;
+
+            this.inputValue = this.textIsValue ? _item.label : _item.value;
+
+            this.validate();
+
+            // this.$emit('input', this.inputValue);
+            /*if (!this.inlineAutocomplete || _selectionAction !== 'enterKey') {
+                this.emitChangeEvent(_item, _selectionAction);
+            }*/
+
+            if (!this.inlineAutocomplete || _selectionAction === 'click' || _selectionAction === 'enterKey') {
+                this.hidePopover();
+            }
+
+            this.$nextTick(() => {
+                this._listboxItemSelected = false;
+            });
+        },
+
+        /**
+         * @param {FListboxItem} _item
+         */
+        onListboxItemFocus(_item) {
+            this.activeDescendant = _item.id || '';
+        },
+
+        onLoading(_loading) {
+            this.loading = _loading;
+        },
+
+        onInputKeydown(_event) {
+            const { listbox } = this.$refs;
+
+            // console.log(_event.key);
+            if (isKey('ArrowDown', _event)) {
+                /*if (this.readonly) {
+                    this.listboxData = clone(this.data);
+                }*/
+
+                this.showPopover();
+            }
+            if (isKey('Enter', _event)) {
+                if (this.popoverVisible) {
+                    _event.preventDefault();
+                }
+            } else if (isKey('Escape', _event)) {
+                this.hidePopover();
+
+                if (this.selectMode) {
+                    this.iValue = this.selectedItem.label;
+                } else {
+                    this.iValue = '';
+                }
+            }
+
+            if (listbox) {
+                listbox.onKeydown(_event, this.readonly);
+            }
+        },
+
+        onInputKeyup(_event) {
+            const { listbox } = this.$refs;
+
+            if (listbox) {
+                listbox.onKeyup(_event);
+            }
+        },
+
+        onClick() {
+            this.onButtonClick();
+        },
+
+        onButtonClick() {
+            const { input } = this.$refs;
+
+            if (this.disabled) {
+                return;
+            }
+
+            // if always show
+            this.listboxData = isPromise(this.data) ? this.data : clone(this.data);
+
+            this.showPopover();
+
+            if (input && !this.readonly) {
+                input.select();
+            }
+        },
+
+        onInputFocus() {
+            this.hasFocus = true;
+        },
+
+        onInputBlur() {
+            if (this.selectMode) {
+                this.iValue = this.selectedItem.label;
+            }
+
+            this.hidePopover();
+            this.hasFocus = false;
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+@use "style";
+</style>
