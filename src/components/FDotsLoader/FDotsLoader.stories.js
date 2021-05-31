a4fd6c4812d0348944cfdace3fcb1979f51a63c5
Pavel Spacil <pavel.spacil@gmail.com>
2021-05-31 07:57:36 +0200

    feat(f-dots-loader): create component

---
+++ src/components/FDotsLoader/FDotsLoader.stories.js
@@ -0,0 +1,48 @@
+import { withA11y } from '@storybook/addon-a11y';
+import FDotsLoader from '@/components/FDotsLoader/FDotsLoader.vue';
+
+export default {
+    title: 'FDotsLoader',
+    component: FDotsLoader,
+    decorators: [withA11y],
+};
+
+export const Default = () => ({
+    components: { FDotsLoader },
+    template: `
+        <div>
+            <f-dots-loader />
+        </div>
+    `,
+});
+
+export const Color = () => ({
+    components: { FDotsLoader },
+    template: `
+        <div class="flex ali-center gap-5">
+            <f-dots-loader color="crimson" />
+            <f-dots-loader color="magenta" />
+        </div>
+    `,
+});
+
+export const Size = () => ({
+    components: { FDotsLoader },
+    template: `
+        <div class="flex ali-center gap-5">
+            <f-dots-loader size="24px" />
+            <f-dots-loader />
+            <f-dots-loader size="3px" />
+        </div>
+    `,
+});
+
+export const Speed = () => ({
+    components: { FDotsLoader },
+    template: `
+        <div class="flex ali-center gap-5">
+            <f-dots-loader speed="400ms" />
+            <f-dots-loader speed="2s" />
+        </div>
+    `,
+});
---
+++ src/components/FDotsLoader/FDotsLoader.vue
@@ -0,0 +1,77 @@
+<template>
+    <span class="fdotsloader">
+        <span></span>
+        <span></span>
+        <span></span>
+    </span>
+</template>
+
+<script>
+/**
+ * Three dots loader component
+ */
+export default {
+    name: 'FDotsLoader',
+
+    props: {
+        color: {
+            type: String,
+            default: '',
+        },
+        size: {
+            type: String,
+            default: '',
+        },
+        speed: {
+            type: String,
+            default: '',
+        },
+    },
+
+    watch: {
+        color(_value) {
+            this.setColor(_value);
+        },
+
+        size(_value) {
+            this.setSize(_value);
+        },
+
+        speed(_value) {
+            this.setSpeed(_value);
+        },
+    },
+
+    mounted() {
+        this.setColor(this.color);
+        this.setSize(this.size);
+        this.setSpeed(this.speed);
+    },
+
+    methods: {
+        setCustomProperty(_prop, _value) {
+            const { $el } = this;
+
+            if ($el && _value) {
+                $el.style.setProperty(_prop, _value);
+            }
+        },
+
+        setColor(_value) {
+            this.setCustomProperty('--fdotsloader-background', _value);
+        },
+
+        setSize(_value) {
+            this.setCustomProperty('--fdotsloader-size', _value);
+        },
+
+        setSpeed(_value) {
+            this.setCustomProperty('--fdotsloader-anim-duration', _value);
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+@use "style";
+</style>
