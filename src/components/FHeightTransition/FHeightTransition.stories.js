3c50f18740283448319aa0fadd08eaa003d0f053
Pavel Spacil <pavel.spacil@gmail.com>
2021-07-13 09:44:48 +0200

    feat(f-height-transition): create component

---
+++ src/components/FHeightTransition/FHeightTransition.stories.js
@@ -0,0 +1,76 @@
+import FHeightTransition from './FHeightTransition.vue';
+import FButton from '@/components/FButton/FButton.vue';
+
+export default {
+    title: 'FHeightTransition',
+    component: FHeightTransition,
+};
+
+export const Default = () => ({
+    components: { FHeightTransition, FButton },
+    template: `
+        <div style="max-width: 400px">
+            <f-button :label="!expand ? 'Expand' : 'Collapse'" @click.native="expand = !expand" class="mab-3" />
+
+            <f-height-transition>
+                <div v-if="expand" id="elem">
+                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda delectus deserunt dignissimos dolores facere, fuga id in incidunt laudantium nisi numquam officia quam soluta totam unde voluptate. Asperiores, vero!</div>
+                    <div>Eligendi facere in possimus quae sunt tempora veritatis! Accusantium architecto commodi consectetur culpa, hic ipsam libero magni maxime nulla, numquam odio omnis quas quos sed sint tempora voluptate? Debitis, molestiae?</div>
+                </div>
+            </f-height-transition>
+
+            <hr>
+        </div>
+    `,
+    data() {
+        return {
+            expand: false,
+        };
+    },
+});
+
+export const TransitionOptions = () => ({
+    components: { FHeightTransition, FButton },
+    template: `
+        <div style="max-width: 400px">
+            <f-button :label="!expand ? 'Expand' : 'Collapse'" @click.native="expand = !expand" class="mab-3" />
+
+            <f-height-transition transition-length="1s" transition-func="ease-in-out">
+                <div v-if="expand" id="elem">
+                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda delectus deserunt dignissimos dolores facere, fuga id in incidunt laudantium nisi numquam officia quam soluta totam unde voluptate. Asperiores, vero!</div>
+                    <div>Eligendi facere in possimus quae sunt tempora veritatis! Accusantium architecto commodi consectetur culpa, hic ipsam libero magni maxime nulla, numquam odio omnis quas quos sed sint tempora voluptate? Debitis, molestiae?</div>
+                </div>
+            </f-height-transition>
+
+            <hr>
+        </div>
+    `,
+    data() {
+        return {
+            expand: false,
+        };
+    },
+});
+
+export const Disabled = () => ({
+    components: { FHeightTransition, FButton },
+    template: `
+        <div style="max-width: 400px">
+            <f-button :label="!expand ? 'Expand' : 'Collapse'" @click.native="expand = !expand" class="mab-3" />
+
+            <f-height-transition disabled>
+                <div v-if="expand" id="elem">
+                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda delectus deserunt dignissimos dolores facere, fuga id in incidunt laudantium nisi numquam officia quam soluta totam unde voluptate. Asperiores, vero!</div>
+                    <div>Eligendi facere in possimus quae sunt tempora veritatis! Accusantium architecto commodi consectetur culpa, hic ipsam libero magni maxime nulla, numquam odio omnis quas quos sed sint tempora voluptate? Debitis, molestiae?</div>
+                </div>
+            </f-height-transition>
+
+            <hr>
+        </div>
+    `,
+    data() {
+        return {
+            expand: false,
+        };
+    },
+});
---
+++ src/components/FHeightTransition/FHeightTransition.vue
@@ -0,0 +1,96 @@
+<template>
+    <transition
+        :name="!disabled ? 'fheighttransition' : ''"
+        @before-enter="onBeforeEnter"
+        @enter="onEnter"
+        @after-enter="onAfterEnter"
+        @before-leave="onBeforeLeave"
+        @after-leave="onAfterLeave"
+    >
+        <slot></slot>
+    </transition>
+</template>
+
+<script>
+import { defer } from '../../utils/index.js';
+import { setCustomProperty } from '../../utils/css.js';
+
+/**
+ * Animate height of an element from height `0` (hidden) to `auto` and vice-versa
+ */
+export default {
+    name: 'FHeightTransition',
+
+    props: {
+        transitionLength: {
+            type: String,
+            default: '',
+        },
+        transitionFunc: {
+            type: String,
+            default: '',
+        },
+        /** Disable animation */
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+    },
+
+    methods: {
+        setTransitionOptions(elem) {
+            if (this.transitionLength) {
+                setCustomProperty('--fheighttransition-transition-length', this.transitionLength, elem);
+            }
+
+            if (this.transitionFunc) {
+                setCustomProperty('--fheighttransition-transition-func', this.transitionFunc, elem);
+            }
+        },
+
+        onBeforeEnter(elem) {
+            this.setTransitionOptions(elem);
+
+            elem.classList.add('fheighttransition-willchange');
+        },
+
+        onEnter(elem) {
+            const height = elem.scrollHeight;
+
+            elem.style.height = 0;
+            // force browser to repaint
+            elem.offsetHeight;
+
+            defer(() => {
+                elem.style.height = `${height}px`;
+            });
+        },
+
+        onAfterEnter(elem) {
+            elem.style.height = 'auto';
+            elem.classList.remove('willchange');
+        },
+
+        onBeforeLeave(elem) {
+            const height = elem.scrollHeight;
+
+            elem.style.height = `${height}px`;
+            elem.offsetHeight;
+            elem.classList.add('willchange');
+
+            defer(() => {
+                elem.style.height = 0;
+            });
+        },
+
+        onAfterLeave(elem) {
+            elem.style.height = 'auto';
+            elem.classList.remove('fheighttransition-willchange');
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+@use "style";
+</style>
