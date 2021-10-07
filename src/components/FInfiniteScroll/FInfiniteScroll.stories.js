400b75cb21a6ca2bc8ea96c78baa9fb0e090d59f
Pavel Spacil <pavel.spacil@gmail.com>
2021-10-07 15:35:23 +0200

    feat(f-infinite-scroll): create component

---
+++ src/components/FInfiniteScroll/FInfiniteScroll.stories.js
@@ -0,0 +1,265 @@
+import FInfiniteScroll from '@/components/FInfiniteScroll/FInfiniteScroll.vue';
+
+export default {
+    title: 'FInfiniteScroll',
+    component: FInfiniteScroll,
+};
+
+function getData(idPrefix = 'i', totalItems = 10) {
+    const data = [];
+
+    for (let i = 1; i < totalItems; i++) {
+        data.push({
+            id: `${idPrefix}_${i}`,
+            text: `--- ${i} ---- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut dolorum et inventore quidem unde, vitae? Aliquid, cupiditate error eveniet impedit iste maiores nemo porro provident quasi quod totam voluptate? Asperiores consequatur culpa deserunt doloremque eius excepturi exercitationem iusto molestias, nihil officiis perferendis possimus, quo, sapiente sequi ut. Ad blanditiis consectetur ea exercitationem fugiat laborum maiores maxime, nostrum numquam! Molestias? Ab alias, amet at deleniti fugit ipsa laboriosam laudantium libero maxime modi molestias natus nihil, odio officiis repudiandae sapiente sequi, sint suscipit vitae voluptatem! Assumenda consectetur eius eligendi nulla velit? Architecto cum error provident quae repudiandae soluta vero. Ad aliquid asperiores assumenda distinctio dolor ex facere illum libero magni natus neque, officia officiis perspiciatis possimus quia sapiente veritatis vitae voluptate. --- /${i} ----`,
+        });
+    }
+
+    return data;
+}
+
+function fetchPagedData(timeout = 1000, pagination, idPrefix = 'i', totalItems = 10) {
+    return new Promise(resolve =>
+        setTimeout(() => {
+            let cData = getData(idPrefix, totalItems);
+
+            resolve(
+                cData.slice((pagination.currPage - 1) * pagination.perPage, pagination.currPage * pagination.perPage)
+            );
+        }, timeout)
+    );
+}
+
+export const Default = () => ({
+    components: { FInfiniteScroll },
+    template: `
+        <div style="max-width: 400px; margin: 0 auto">
+            <p style="position: fixed; left: 0; top: 0;"><b v-show="loading">Loading...</b></p>
+            <f-infinite-scroll :total-items="20" :per-page="5" :curr-page="1" :loading="loading" @page-change="onPageChange">
+                <p v-for="item in data" :key="item.id">{{ item.text }}</p>
+            </f-infinite-scroll>
+        </div>
+    `,
+    data() {
+        return {
+            data: [],
+            loading: false,
+        };
+    },
+    methods: {
+        async onPageChange(pagination) {
+            this.loading = true;
+
+            const data = await fetchPagedData(1000, pagination, 'i1', 20);
+
+            if (pagination.currPage < pagination.prevPage) {
+                this.data = data.concat(this.data);
+            } else {
+                this.data = this.data.concat(data);
+            }
+
+            this.loading = false;
+        },
+    },
+});
+
+export const MiddlePage = () => ({
+    components: { FInfiniteScroll },
+    template: `
+        <div style="max-width: 400px; margin: 0 auto">
+            <p style="position: fixed; left: 0; top: 0;"><b v-show="loading">Loading...</b></p>
+            <f-infinite-scroll :total-items="20" :per-page="5" :curr-page="3" :loading="loading" @page-change="onPageChange">
+                <p v-for="item in data" :key="item.id">{{ item.text }}</p>
+            </f-infinite-scroll>
+        </div>
+    `,
+    data() {
+        return {
+            data: [],
+            loading: false,
+        };
+    },
+    methods: {
+        async onPageChange(pagination) {
+            this.loading = true;
+
+            const data = await fetchPagedData(1000, pagination, 'i2', 20);
+
+            if (pagination.currPage < pagination.prevPage) {
+                this.data = data.concat(this.data);
+            } else {
+                this.data = this.data.concat(data);
+            }
+
+            this.loading = false;
+        },
+    },
+});
+
+export const LastPage = () => ({
+    components: { FInfiniteScroll },
+    template: `
+        <div style="max-width: 400px; margin: 0 auto">
+            <p style="position: fixed; left: 0; top: 0;"><b v-show="loading">Loading...</b></p>
+            <f-infinite-scroll :total-items="20" :per-page="5" :curr-page="4" :loading="loading" @page-change="onPageChange">
+                <p v-for="item in data" :key="item.id">{{ item.text }}</p>
+            </f-infinite-scroll>
+        </div>
+    `,
+    data() {
+        return {
+            data: [],
+            loading: false,
+        };
+    },
+    methods: {
+        async onPageChange(pagination) {
+            console.log('onPageChange', JSON.stringify(pagination));
+            this.loading = true;
+
+            const data = await fetchPagedData(1000, pagination, 'i3', 20);
+
+            if (pagination.currPage < pagination.prevPage) {
+                this.data = data.concat(this.data);
+            } else {
+                this.data = this.data.concat(data);
+            }
+
+            this.loading = false;
+        },
+    },
+});
+
+export const PerPage1 = () => ({
+    components: { FInfiniteScroll },
+    template: `
+        <div style="max-width: 600px; margin: 0 auto">
+            <p style="position: fixed; left: 0; top: 0;"><b v-show="loading">Loading...</b></p>
+            <f-infinite-scroll :total-items="20" :per-page="1" :curr-page="10" :loading="loading" @page-change="onPageChange">
+                <p v-for="item in data" :key="item.id">{{ item.text }}</p>
+            </f-infinite-scroll>
+        </div>
+    `,
+    data() {
+        return {
+            data: [],
+            loading: false,
+        };
+    },
+    methods: {
+        async onPageChange(pagination) {
+            console.log('onPageChange', JSON.stringify(pagination));
+            this.loading = true;
+
+            const data = await fetchPagedData(500, pagination, 'i4', 20);
+
+            if (pagination.currPage < pagination.prevPage) {
+                this.data = data.concat(this.data);
+            } else {
+                this.data = this.data.concat(data);
+            }
+
+            this.loading = false;
+        },
+    },
+});
+
+export const RootMiddlePage = () => ({
+    components: { FInfiniteScroll },
+    template: `
+        <div id="fio_root" style="max-width: 400px; margin: 0 auto; height: 400px; overflow: auto">
+            <p style="position: fixed; left: 0; top: 0;"><b v-show="loading">Loading...</b></p>
+            <f-infinite-scroll root="#fio_root" :total-items="20" :per-page="5" :curr-page="3" :loading="loading" @page-change="onPageChange">
+                <p v-for="item in data" :key="item.id">{{ item.text }}</p>
+            </f-infinite-scroll>
+        </div>
+    `,
+    data() {
+        return {
+            data: [],
+            loading: false,
+        };
+    },
+    methods: {
+        async onPageChange(pagination) {
+            this.loading = true;
+
+            const data = await fetchPagedData(1000, pagination, 'i5', 20);
+
+            if (pagination.currPage < pagination.prevPage) {
+                this.data = data.concat(this.data);
+            } else {
+                this.data = this.data.concat(data);
+            }
+
+            this.loading = false;
+        },
+    },
+});
+
+export const RootMargin = () => ({
+    components: { FInfiniteScroll },
+    template: `
+        <div id="fio_root" style="max-width: 400px; margin: 0 auto; height: 400px; overflow: auto">
+            <p style="position: fixed; left: 0; top: 0;"><b v-show="loading">Loading...</b></p>
+            <f-infinite-scroll root-margin="400px 0px" root="#fio_root" :total-items="20" :per-page="5" :curr-page="3" :loading="loading" @page-change="onPageChange">
+                <p v-for="item in data" :key="item.id">{{ item.text }}</p>
+            </f-infinite-scroll>
+        </div>
+    `,
+    data() {
+        return {
+            data: [],
+            loading: false,
+        };
+    },
+    methods: {
+        async onPageChange(pagination) {
+            this.loading = true;
+
+            const data = await fetchPagedData(1000, pagination, 'i6', 20);
+
+            if (pagination.currPage < pagination.prevPage) {
+                this.data = data.concat(this.data);
+            } else {
+                this.data = this.data.concat(data);
+            }
+
+            this.loading = false;
+        },
+    },
+});
+
+export const LoaderSlot = () => ({
+    components: { FInfiniteScroll },
+    template: `
+        <div style="max-width: 400px; margin: 0 auto">
+            <p style="position: fixed; left: 0; top: 0;"><b v-show="loading">Loading...</b></p>
+            <f-infinite-scroll :total-items="20" :per-page="5" :curr-page="3" :loading="loading" @page-change="onPageChange">
+                <template #loader><div style="text-align: center; font-weight: bold">Loading...</div></template>
+                <p v-for="item in data" :key="item.id">{{ item.text }}</p>
+            </f-infinite-scroll>
+        </div>
+    `,
+    data() {
+        return {
+            data: [],
+            loading: false,
+        };
+    },
+    methods: {
+        async onPageChange(pagination) {
+            this.loading = true;
+
+            const data = await fetchPagedData(1000, pagination, 'i7', 20);
+
+            if (pagination.currPage < pagination.prevPage) {
+                this.data = data.concat(this.data);
+            } else {
+                this.data = this.data.concat(data);
+            }
+
+            this.loading = false;
+        },
+    },
+});
---
+++ src/components/FInfiniteScroll/FInfiniteScroll.vue
@@ -0,0 +1,235 @@
+<template>
+    <div class="finfinitescroll">
+        <f-intersection-observer
+            v-show="showTopLoader"
+            :root="root"
+            :root-margin="rootMargin"
+            :ignore-first-entry="ignoreFirstEntry"
+            @entry="onTopLoaderEntry"
+        >
+            <slot name="loader">
+                <div class="finfinitescroll_loader">Top <f-dots-loader /></div>
+            </slot>
+        </f-intersection-observer>
+
+        <slot></slot>
+
+        <f-intersection-observer
+            v-show="showBottomLoader"
+            :root="root"
+            :root-margin="rootMargin"
+            :ignore-first-entry="ignoreFirstEntry"
+            @entry="onBottomLoaderEntry"
+        >
+            <slot name="loader">
+                <div class="finfinitescroll_loader">Bottom <f-dots-loader /></div>
+            </slot>
+        </f-intersection-observer>
+
+        <f-pagination
+            ref="pagination"
+            :total-items="dTotalItems"
+            :per-page="perPage"
+            :curr-page="currPage"
+            @page-change="onPageChange"
+            style="display: none"
+            hidden
+        />
+    </div>
+</template>
+
+<script>
+import FIntersectionObserver from '../FIntersectionObserver/FIntersectionObserver.vue';
+import FDotsLoader from '../FDotsLoader/FDotsLoader.vue';
+import FPagination from '../FPagination/FPagination.vue';
+import { clone, defer } from '../../utils/index.js';
+
+export default {
+    name: 'FInfiniteScroll',
+
+    components: { FPagination, FDotsLoader, FIntersectionObserver },
+
+    props: {
+        loading: {
+            type: Boolean,
+            default: false,
+        },
+        ignoreFirstPageChange: {
+            type: Boolean,
+            default: false,
+        },
+        /** Total amount of items (FPagination prop) */
+        totalItems: { ...FPagination.props.totalItems },
+        /** Number of items per page (FPagination prop) */
+        perPage: { ...FPagination.props.perPage },
+        /** Current page number (FPagination prop) */
+        currPage: { ...FPagination.props.currPage },
+        ...FIntersectionObserver.props,
+    },
+
+    data() {
+        return {
+            pagination: {},
+            firstPage: false,
+            topIntersection: false,
+            bottomIntersection: false,
+            prevScrollHeight: 0,
+            lastTopPage: this.currPage,
+            lastBottomPage: this.currPage,
+            eRoot: null,
+            firstChange: true,
+            dTotalItems: this.totalItems,
+            dIgnoreFirstPageChange: this.ignoreFirstPageChange,
+        };
+    },
+
+    computed: {
+        showTopLoader() {
+            return !this.firstChange && !(this.firstPage && !this.loading) && !this.bottomIntersection;
+        },
+
+        showBottomLoader() {
+            let pagination = this.getPaginationState();
+
+            if (pagination.numPages === undefined) {
+                pagination = this.pagination;
+            }
+
+            return !((pagination.isLastPage || this.lastBottomPage >= pagination.numPages) && !this.loading);
+        },
+    },
+
+    watch: {
+        loading(value) {
+            if (!value) {
+                if (this.topIntersection) {
+                    defer(() => {
+                        this.scrollTo(this.eRoot.scrollHeight - this.prevScrollHeight);
+                    });
+                }
+
+                defer(() => {
+                    defer(() => {
+                        if (this.bottomIntersection && this.showBottomLoader) {
+                            this.goToPage('next');
+                        }
+                    });
+                });
+            } else {
+                this.prevScrollHeight = this.eRoot.scrollHeight;
+            }
+        },
+
+        totalItems(value) {
+            this.dTotalItems = value;
+        },
+    },
+
+    mounted() {
+        this.eRoot = this.root ? document.querySelector(this.root) : document.documentElement;
+
+        if (!this.eRoot) {
+            throw new Error("Can't find root element");
+        }
+
+        this.setScrollTop();
+    },
+
+    methods: {
+        /**
+         * @param {number|'next'|'prev'} page
+         */
+        goToPage(page) {
+            const { pagination } = this.$refs;
+            let p = 0;
+
+            if (typeof page === 'number') {
+                this.lastTopPage = page;
+                this.lastBottomPage = page;
+                p = page;
+            } else if (page === 'next') {
+                this.lastBottomPage += 1;
+                p = this.lastBottomPage;
+            } else {
+                // prev
+                this.lastTopPage -= 1;
+                p = this.lastTopPage;
+            }
+
+            if (pagination) {
+                pagination.goToPage(p);
+            }
+        },
+
+        getPaginationState() {
+            const { pagination } = this.$refs;
+
+            if (pagination) {
+                return pagination.state;
+            }
+
+            return {};
+        },
+
+        /**
+         * @param {number} scrollTop
+         */
+        scrollTo(scrollTop) {
+            this.eRoot.scrollTop = scrollTop;
+        },
+
+        setScrollTop() {
+            this.prevScrollHeight = this.eRoot.scrollHeight;
+        },
+
+        /**
+         * Triggered on `FPagination`'s `'page-change'` event.
+         *
+         * @param {FPaginationState} state
+         */
+        onPageChange(state) {
+            const pagination = clone(state);
+
+            if (pagination.totalItems <= 0) {
+                return;
+            }
+
+            if (!this.firstPage && (pagination.isFirstPage || pagination.prevPage === 1)) {
+                this.firstPage = true;
+            }
+
+            if (!this.dIgnoreFirstPageChange) {
+                this.pagination = pagination;
+
+                if (this.firstChange) {
+                    this.firstChange = false;
+                }
+
+                this.$emit('page-change', { ...pagination });
+            } else {
+                this.dIgnoreFirstPageChange = false;
+            }
+        },
+
+        onTopLoaderEntry(entry) {
+            this.topIntersection = entry.isIntersecting;
+
+            if (!this.loading && this.topIntersection) {
+                this.goToPage('prev');
+            }
+        },
+
+        onBottomLoaderEntry(entry) {
+            this.bottomIntersection = entry.isIntersecting;
+
+            if (!this.loading && this.bottomIntersection) {
+                this.goToPage('next');
+            }
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+@use "style";
+</style>
